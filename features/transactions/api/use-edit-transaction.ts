d192cbd1b10219caf0caa9dc2d90deb13893440c
Angad Anil Gosain <angadgosain@gmail.com>
2024-06-27 02:04:13 -0400

    Developmed hooks requried for Transaction API

---
+++ features/transactions/api/use-edit-transaction.ts
@@ -0,0 +1,37 @@
+import { toast } from "sonner";
+
+import { InferRequestType, InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import {client} from "@/lib/hono"
+
+type ResponseType = InferResponseType<typeof client.api.transactions[":id"]["$patch"]>;
+type RequestType = InferRequestType<typeof client.api.transactions[":id"]["$patch"]>["json"];
+
+export const useEditTransaction = (id?: string) => {
+    const queryCLient = useQueryClient();
+
+    const mutation = useMutation<
+        ResponseType,
+        Error,
+        RequestType
+    >({
+        mutationFn: async(json) => {
+            const response = await client.api.transactions[":id"]["$patch"]({
+                json,
+                param: {id}
+            });
+            return await response.json();
+        },
+        onSuccess: () => {
+            toast.success("Transaction Updated");
+            queryCLient.invalidateQueries({queryKey: ["transaction", {id}]});
+            queryCLient.invalidateQueries({queryKey: ["transactions"]});
+            // TODO: Invalidate Summary
+        },
+        onError: () => {
+            toast.error("Failed to edit Transaction");
+        }
+    });
+    return mutation;
+}
